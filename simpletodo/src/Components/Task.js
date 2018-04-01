5f5298adf4e7580794cd51a413fd41ebfc35e05b
Al <ismail3li@gmail.com>
2018-04-01 12:33:27 +0200

    Text Decoration and Boolean use from props

--- simpletodo/src/Components/Task.js
+++ simpletodo/src/Components/Task.js
@@ -2,14 +2,18 @@
  * @Author: Ali Ismail
  * @Date:   2018-03-31T11:02:15+02:00
  * @Last modified by:   Ali Ismail
- * @Last modified time: 2018-03-31T15:37:05+02:00
+ * @Last modified time: 2018-04-01T12:31:04+02:00
  */
  import React, { Component } from 'react';
  class Task extends Component {
    render() {
      return (
        <div className="Task">
-       <span>{this.props.todo.value}</span>
+       <span
+         style={{ textDecoration: this.props.todo.done ? 'line-through' : 'none'}}>
+         {this.props.todo.value}
+       </span>
+       <button>{this.props.todo.done ? 'undo' : 'Complete'}</button>>
        </div>
      )
    }
